e73c9506effadf08a2708220e6f6bd117a9a8326
tomassio <royswiss@hotmail.com>
2024-05-30 12:55:12 +0200

    Kafka service
    Kafka service

---
+++ MessageService/src/SimpleProducer.java
@@ -0,0 +1,16 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import java.util.Properties;
+
+public class SimpleProducer {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+        producer.send(new ProducerRecord<>("my-topic", "key", "value"));
+        producer.close();
+    }
+}
