ec815198b2a59f73b5996aa4453eba095036d425
tomassio <royswiss@hotmail.com>
2024-05-30 15:14:56 +0200

    duplicate

--- demo/TestDB.java
+++
@@ -1,8 +0,0 @@
-package demo;
-
-public class TestDB {
-    public static void main(String[] args) {	
-	DatabaseConnection connection1 = DatabaseConnection.getInstance();
-	connection1.query("Thomas");
-    }
-}
